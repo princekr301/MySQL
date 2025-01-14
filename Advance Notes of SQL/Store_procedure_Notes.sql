8a30f6bf2cabc1352d4248c48b7568690bcb1bce
Prince kumar <114976085+princekr301@users.noreply.github.com>
2025-01-15 00:08:08 +0530

    Store Procedure

---
+++ Advance Notes of SQL/Store_procedure_Notes.sql
@@ -0,0 +1,28 @@
+
+
+#    --------------------------------------------------------------------------------------------------
+#    -------------------------------- Store Procedure   ----------------------------------------
+#    --------------------------------------------------------------------------------------------------
+
+-- Stored Procedures are a powerful feature in SQL that allows you to encapsulate a set of SQL statements 
+-- for reuse, maintainability, and performance optimization. They are essentially precompiled collections
+--  of SQL statements that can be executed as a single unit.
+
+
+#  Definition:
+# -------------------------------------------------------------------------------------------------
+
+-- A stored procedure is a saved collection of SQL queries that can be reused multiple times.
+-- They are stored in the database and can be executed with a single call.
+
+# Benefits:
+#--------------------------------------------------------------------------------------------------
+
+-- Reusability: Write once, use multiple times.
+-- Maintainability: Easier to update and manage logic in one place.
+-- Performance: Precompiled and optimized by the database engine.
+-- Security: Can restrict direct access to data, exposing only necessary operations.
+
+
+
+
